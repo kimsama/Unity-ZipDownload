30ea5be1315c227634eb090b0921563adf4b7878
kimsama <hwpeterkim>
2016-04-15 11:39:07 +0900

    - Removed tabs. \n - Replaced Debug.Log with Debug.LogFormat

--- Assets/Script/ZipFile.cs
+++ Assets/Script/ZipFile.cs
@@ -8,60 +8,59 @@ using ICSharpCode.SharpZipLib.Zip;
 /// </summary>
 public class ZipFile 
 {
-	/// <summary>
-	/// Write the given bytes data under the given filePath. 
-	/// The filePath should be given with its path and filename. (e.g. c:/tmp/test.zip)
-	/// </summary>
-	public static void UnZip(string filePath, byte[] data)
-	{
-		using (ZipInputStream s = new ZipInputStream(new MemoryStream(data)))
-		{
-			ZipEntry theEntry;
-			while ((theEntry = s.GetNextEntry()) != null)
-			{
-				#if UNITY_EDITOR
-				Debug.Log("Entry Name: " + theEntry.Name);
-				#endif
+    /// <summary>
+    /// Write the given bytes data under the given filePath. 
+    /// The filePath should be given with its path and filename. (e.g. c:/tmp/test.zip)
+    /// </summary>
+    public static void UnZip(string filePath, byte[] data)
+    {
+        using (ZipInputStream s = new ZipInputStream(new MemoryStream(data)))
+        {
+            ZipEntry theEntry;
+            while ((theEntry = s.GetNextEntry()) != null)
+            {
+                #if UNITY_EDITOR
+                Debug.LogFormat("Entry Name: {0}", theEntry.Name);
+                #endif
 
-				string directoryName = Path.GetDirectoryName(theEntry.Name);
-				string fileName = Path.GetFileName(theEntry.Name);
+                string directoryName = Path.GetDirectoryName(theEntry.Name);
+                string fileName = Path.GetFileName(theEntry.Name);
 
-				// create directory
-				if (directoryName.Length > 0)
-				{
-					var dirPath = Path.Combine (filePath, directoryName);
+                // create directory
+                if (directoryName.Length > 0)
+                {
+                    var dirPath = Path.Combine (filePath, directoryName);
 
-					#if UNITY_EDITOR
-					Debug.Log ("CreateDirectory: " + dirPath);
-					#endif
+                    #if UNITY_EDITOR
+                    Debug.LogFormat("CreateDirectory: {0}", dirPath);
+                    #endif
 
-					Directory.CreateDirectory(dirPath);
-				}
+                    Directory.CreateDirectory(dirPath);
+                }
 
-				if (fileName != string.Empty)
-				{
-					// retrieve directory name only from persistence data path.
-					var entryFilePath = Path.Combine(filePath, theEntry.Name);
-					using (FileStream streamWriter = File.Create(entryFilePath))
-					{
-						int size = 2048;
-						byte[] fdata = new byte[size];
-						while (true)
-						{
-							size = s.Read(fdata, 0, fdata.Length);
-							if (size > 0)
-							{
-								streamWriter.Write(fdata, 0, size);
-							}
-							else
-							{
-								break;
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
+                if (fileName != string.Empty)
+                {
+                    // retrieve directory name only from persistence data path.
+                    var entryFilePath = Path.Combine(filePath, theEntry.Name);
+                    using (FileStream streamWriter = File.Create(entryFilePath))
+                    {
+                        int size = 2048;
+                        byte[] fdata = new byte[size];
+                        while (true)
+                        {
+                            size = s.Read(fdata, 0, fdata.Length);
+                            if (size > 0)
+                            {
+                                streamWriter.Write(fdata, 0, size);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            } //end of while
+        } //end of using
+    }
 }
